ea32a196d18fc9a1e3dddcf02e4c154d427f7e0e
Michael Volkmann <michael.volkmann@hs-mittweida.de>
2012-02-24 17:16:46 +0100

    Pull Configuration - all Subprojects now pull thier config from onfiguration - clean the pulled stuff up with ant clean.all
    Signed-off-by: Michael Volkmann <michael.volkmann@hs-mittweida.de>

---
+++ configuration/version.js
@@ -0,0 +1,3 @@
+if(!Signavio){ var Signavio = {} };
+	if (!Signavio.Core) { Signavio.Core = {} };
+	Signavio.Core.Version = "${version}";
---
+++ explorer/one-war.js
@@ -0,0 +1,15 @@
+Signavio.Config.EXPLORER_PATH		= '/${war}/explorer';
+Signavio.Config.EDITOR_PATH			= '/${war}/editor';
+Signavio.Config.BACKEND_PATH		= '/${war}/p';
+Signavio.Config.LIBS_PATH			= '/${war}/libs';
+
+Signavio.Config.DIAGRAMS_IMAGE_PATH = Signavio.Config.EXPLORER_PATH + "/src/img"
+Signavio.Config.STENCILSET_EXTENSION_PATH = Signavio.Config.EDITOR_PATH + "/stencilsets/extensions/";
+Signavio.Config.PATH 				= Signavio.Config.EXPLORER_PATH + '/src/javascript';
+Signavio.Config.PLUGIN_PATH 		= Signavio.Config.EXPLORER_PATH + '/src/javascript/plugins';
+Signavio.Config.EDITOR_HANDLER_URI 	= Signavio.Config.BACKEND_PATH + "/editor";
+Signavio.Config.STENCILSET_URI		= Signavio.Config.BACKEND_PATH + "/editor_stencilset";
+Signavio.Config.PLUGIN_CONFIG 		= Signavio.Config.BACKEND_PATH + "/explorer_plugins";
+Signavio.Config.USE_CACHE			= false;
+Signavio.Config.REMOVE_ON_DELETE	= true;
+Signavio.Config.HIDE_NOTIFICATION	= true;
